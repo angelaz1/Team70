e8327bb9bd250ca1b8ac22b2e49bdae27356336a
guidaohuangxing <guidao12@163.com>
2021-10-21 14:07:42 -0400

    door

---
+++ Team70/Assets/Scripts/Yiming/Door.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]Animator doorAni;
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Dog")
+        {
+            
+            doorAni.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Dog")
+        {
+            doorAni.SetBool("isOpen", false);
+        }
+    }
+}
