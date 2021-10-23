e8d9b91ca38abadf250d1a187592e8821b06aeaf
guidaohuangxing <guidao12@163.com>
2021-10-23 17:16:38 -0400

    add go back movement

--- Team70/Assets/Scripts/Yiming/DogMovement.cs
+++ Team70/Assets/Scripts/Yiming/DogMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class DogMovement : MonoBehaviour
 {
@@ -18,10 +19,17 @@ public class DogMovement : MonoBehaviour
     public float MoveSpeed = 10f;
     public float upWardShake = 0.05f;
     public float velocityLimit = 10f;
+
+    private InputDevice rightHand;
+    private bool pressRightGrip = false;
+    private InputDevice leftHand;
+    private bool pressLeftGrip = false;
+    Vector3 normalPlane;
     //public bool isPrepare = false;
     void Start()
     {
-     
+
+        normalPlane = new Vector3(0, 1, 0);
         foreach(var item in pace)
         {
             rightPawMovepace.Add(rightInput.name + item.name);
@@ -29,12 +37,43 @@ public class DogMovement : MonoBehaviour
         }
     
         rigidBody = this.GetComponent<Rigidbody>();
+        InitialDevice();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rightHand.TryGetFeatureValue(CommonUsages.gripButton, out bool rgb))
+        {
+            
+            pressRightGrip = rgb;
+        }
+        if (leftHand.TryGetFeatureValue(CommonUsages.gripButton, out bool lgb))
+        {
+
+            pressLeftGrip = lgb;
+        }
+        print(pressRightGrip);
+    }
+
+
+
+    public void InitialDevice()
+    {
+        List<InputDevice> rightdevices = new List<InputDevice>();
+        List<InputDevice> leftdevices = new List<InputDevice>();
+        InputDeviceRole righthand = InputDeviceRole.RightHanded;
+        InputDeviceRole lefthand = InputDeviceRole.LeftHanded;
+        InputDevices.GetDevicesWithRole(righthand, rightdevices);
+        if (rightdevices.Count > 0)
+        {
+            rightHand = rightdevices[0];
+        }
+        InputDevices.GetDevicesWithRole(lefthand, leftdevices);
+        if (leftdevices.Count > 0)
+        {
+            leftHand = leftdevices[0];
+        }
     }
 
     public void RecieveDogPawsBehavior(string pawsInput)
@@ -51,7 +90,17 @@ public class DogMovement : MonoBehaviour
 
     private void addFroceToDogBody()
     {
-            Vector3 dir = Camera.main.transform.forward;
+            Vector3 dir = Vector3.zero;
+            if(pressRightGrip || pressLeftGrip)
+            {
+                dir = Vector3.ProjectOnPlane(Camera.main.transform.forward, normalPlane) * -1;
+                print(dir);
+            }
+            else
+            {
+                dir = Vector3.ProjectOnPlane(Camera.main.transform.forward, normalPlane);
+                print(dir);
+        } 
             //add small froce toward up
             dir = dir + upWardShake * Vector3.up;
             if(rigidBody.velocity.magnitude < velocityLimit)
