f03fe22bf4b9fec83a60ff8efa5a6d113dd70f9a
Angela Zhang <angelaz1@andrew.cmu.edu>
2021-10-27 18:33:00 -0400

    Messing with placement areas/changing indicator

--- Team70/Assets/Scripts/BGMManager.cs
+++ Team70/Assets/Scripts/BGMManager.cs
@@ -10,6 +10,7 @@ public class BGMManager : MonoBehaviour
     bool fadingIn = true;
 
     float fadeSpeed = 0.4f;
+    public float maxVolume = 0.6f;
 
     void Start()
     {
@@ -21,14 +22,14 @@ public class BGMManager : MonoBehaviour
         if (fadingOut)
         {
             fadingIn = false;
-            audioSource.volume -= fadeSpeed * Time.deltaTime;
+            audioSource.volume = Mathf.Clamp(audioSource.volume - fadeSpeed * Time.deltaTime, 0, maxVolume);
             if (audioSource.volume == 0) fadingOut = false;
         }
 
         else if (fadingIn)
         {
-            audioSource.volume += fadeSpeed * Time.deltaTime;
-            if (audioSource.volume == 1) fadingIn = false;
+            audioSource.volume = Mathf.Clamp(audioSource.volume + fadeSpeed * Time.deltaTime, 0, maxVolume);
+            if (audioSource.volume >= maxVolume) fadingIn = false;
         }
     }
 
